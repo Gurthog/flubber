077c3e9f2adbb681af57139c4aabefbbf77b35e7
Gurthog <226201902+Gurthog@users.noreply.github.com>
2026-08-13 19:55:39 -0600

    add cloudflare settings

--- project/settings.py
+++ project/settings.py
@@ -27,8 +27,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'flubber.icu',
+    'www.flubber.icu',
     'localhost',
+    '127.0.0.1',
 ]
+
+# Cloudflare
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
