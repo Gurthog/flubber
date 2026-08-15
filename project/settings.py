96cc850c87b50a2ff5787112763ba58e6d23bd79
Gurthog <226201902+Gurthog@users.noreply.github.com>
2026-08-15 01:35:25 -0600

    add whitenoise

--- project/settings.py
+++ project/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS.extend([
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
