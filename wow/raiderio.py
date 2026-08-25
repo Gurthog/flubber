59136efbb343a52a5edea74fa8ef148963fee0f7
Gurthog <226201902+Gurthog@users.noreply.github.com>
2026-08-24 22:41:46 -0600

    add mythic season stats

--- wow/raiderio.py
+++ wow/raiderio.py
@@ -32,7 +32,25 @@ def get_character(name: str, realm: str, fields: list[str] = []):
 
     return response.json()
 
+def join_character_mythic_stats(bests: list[dict], counts: list[dict]) -> list[dict]:
+    """
+    Combine data from mythic_plus_best_runs and mythic_plus_dungeon_run_counts.
+    """
+    keys = {"dungeon", "short_name", "mythic_level", "score"}
+    pruned_bests = [
+        {k: run[k] for k in keys}
+        for run in bests
+    ]
+    counts_lookup = {record['short_name']: record for record in counts}
 
+    stats = []
+    for record in pruned_bests:
+        match = counts_lookup[record['short_name']]
+        stats.append({
+            **record,
+            'season_runs_total': match['season_runs_total']
+        })
+    return stats
 
 
 if __name__ == "__main__":
@@ -44,10 +62,20 @@ if __name__ == "__main__":
         'bungle',
         'khaz-modan',
         fields = [
-            'guild',
-            'mythic_plus_dungeon_run_counts',
-            'mythic_plus_recent_runs',
+            # 'guild',
+            'mythic_plus_dungeon_run_counts',   # dungeon, short_name, mythic_level, score
+            'mythic_plus_best_runs',    # dungeon, short_name, season_runs_total
+            # 'mythic_plus_recent_runs',
         ],
     )
-    print(data)
+
+    best_runs: list[dict] = data['mythic_plus_best_runs']
+    run_counts: list[dict] = data['mythic_plus_dungeon_run_counts']
+
+    stats = join_character_mythic_stats(best_runs, run_counts)
+    print(stats)
+
+    score = 0
+    for x in best_runs:
+        score += x['score']
 
--- wow/views.py
+++ wow/views.py
@@ -17,10 +17,16 @@ def bungle(request):
         'khaz-modan',
         fields = [
             'guild',
+            'mythic_plus_best_runs',
             'mythic_plus_dungeon_run_counts',
             'mythic_plus_recent_runs',
         ]
     )
-    context = { 'c': data }
+    stats = raiderio.join_character_mythic_stats(
+        data['mythic_plus_best_runs'],
+        data['mythic_plus_dungeon_run_counts']
+    )
+    mythic_rating = sum(record['score'] for record in stats)
+    context = { 'c': data, 'stats': stats, 'mythic_rating': mythic_rating }
     return render(request, "wow/bungle.html", context)
 
